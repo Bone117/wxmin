33f1e6f778d04297eb3d1f287f878916ad92b757
outFile <785484564@qq.com>
2020-02-04 20:30:13 +0800

    页面交互api练习

---
+++ app.js
@@ -0,0 +1,20 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    wx.login({
+      success(res){
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,46 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '你好啊',
+      icon: 'loading',
+    })
   },
+  handleShowModal(){
+    wx.showModal({
+      title: 'this is title',
+      content: 'this is content',
+      showCancel:false,
+      success(res){
+        console.log(res)
+        if(res.cancel){
+          console.log("点击了取消")
+        }
+        if(res.confirm){
+          console.log("点击了确定")
+        }
+      },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    })
   },
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载中',
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    setTimeout(function(){
+      wx.hideLoading()
+    },2000)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['A','B','C'],
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(){
+    return{
+      title:"你好啊，李银河"
+    }
   }
 })
